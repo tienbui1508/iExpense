bc7a5f312d7351e026faea4660913ff1209262c2
tienbui <tienbui1508@gmail.com>
2023-06-01 19:33:10 +0930

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Tien Bui on 1/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
